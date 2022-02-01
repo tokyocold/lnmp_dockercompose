ac615cf3a00dc98a602b8a95ebcaa3496bdc28a1
lcj <lcj@lcj.com>
2022-02-01 12:42:01 +0800

    .

---
+++ container_config/laravel_boot/boot.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -x
+ 
+
+docker-compose run --rm -w /usr/share/nginx/html/ php composer install || exit 1
+
+docker-compose run --rm -w /usr/share/nginx/html/ php php artisan key:generate || exit 1
+
+docker-compose run --rm -w /usr/share/nginx/html/ php php artisan horizon:install || exit 1
+ 
+docker-compose run --rm -w /usr/share/nginx/html/ php php artisan telescope:install || exit 1
+
+docker-compose run --rm -w /usr/share/nginx/html/ php php artisan storage:link || exit 1
+
+docker run --rm -it -v $(pwd):/app -w /app -e NODE_OPTIONS=--openssl-legacy-provider node yarn || exit 1
+
+docker-compose run --rm -w /usr/share/nginx/html/ php php artisan migrate --seed || exit 1
+
+docker run --rm -it -v $(pwd):/app -w /app -e NODE_OPTIONS=--openssl-legacy-provider node yarn dev  || exit 1
+
